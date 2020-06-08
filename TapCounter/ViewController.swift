cd176cbcfaf556fe9c09f6cecab754e1ab36ea85
Machu Rexford <[rexfordmachu@gmail.com]>
2020-06-08 14:44:09 +0000

    Initial Commit

---
+++ TapCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapCounter
+//
+//  Created by Rexford Machu on 6/8/20.
+//  Copyright © 2020 MachuDigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
